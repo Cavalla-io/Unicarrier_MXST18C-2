275bcb92b552a78f0c1158362f69a7633234cd0f
victor-boyd <victor.w.boyd@gmail.com>
2025-04-25 16:25:54 -0700

    Revert "Fixed build problems"
    This reverts commit 416e7746f9d727dc6123ce609e9500dcd00a4cb9.

--- src/can_bus/setup.py
+++ src/can_bus/setup.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-from setuptools import find_packages, setup
+from setuptools import setup
 import os
 from glob import glob
 
@@ -8,7 +7,7 @@ package_name = 'can_bus'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
